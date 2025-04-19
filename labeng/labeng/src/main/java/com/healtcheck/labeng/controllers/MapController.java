1f6a791eb3037443bdb945dd9390ba9383f5edbe
LuigiBelanda <luigi.belanda.milani@gmail.com>
2025-04-19 08:18:52 -0300

    Ajustando detalhes do controller e map.html

--- labeng/labeng/src/main/java/com/healtcheck/labeng/controllers/MapController.java
+++ labeng/labeng/src/main/java/com/healtcheck/labeng/controllers/MapController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/map")
 @CrossOrigin(origins = "*")
 public class MapController {
     @Autowired
